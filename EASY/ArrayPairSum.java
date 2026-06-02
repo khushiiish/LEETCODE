ccd10fa1aedc453088bb9bb3e0a417c107228d0c
khushisharma2004 <ks969250@gmail.com>
2026-06-02 23:45:02 +0530

    561 array partition

---
+++ EASY/ArrayPairSum.java
@@ -0,0 +1,26 @@
+
+import java.util.Arrays;
+
+class Solution {
+    public int arrayPairSum(int[] nums) {
+        Arrays.sort(nums);
+
+        int sum = 0;
+
+        for (int i = 0; i < nums.length; i += 2) {
+            sum += nums[i];
+        }
+
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        Solution obj = new Solution();
+
+        int[] nums = {1, 4, 3, 2};
+
+        int result = obj.arrayPairSum(nums);
+
+        System.out.println("Output: " + result);
+    }
+}
