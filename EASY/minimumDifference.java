cdda3dd19f1b2144909a804e106304f8a2d9ffee
khushisharma2004 <ks969250@gmail.com>
2026-06-05 22:02:08 +0530

    next greater element 496

--- EASY/minimumDifference.java
+++ EASY/minimumDifference.java
@@ -1,29 +1,32 @@
-
 import java.util.Arrays;
 
-class Solution {
+public class minimumDifference {
+
     public int minimumDifference(int[] nums, int k) {
 
-        // k = 1 hone par difference hamesha 0 hoga
         if (k == 1) {
             return 0;
         }
 
-        // Array sort karke har consecutive group of size k check karte hain
         Arrays.sort(nums);
 
         int minDiff = Integer.MAX_VALUE;
 
-        /*
-         * nums[i] -> group ka smallest element
-         * nums[i + k - 1] -> group ka largest element
-         * Difference nikal kar minimum difference store karte hain
-         */
         for (int i = 0; i <= nums.length - k; i++) {
             int diff = nums[i + k - 1] - nums[i];
             minDiff = Math.min(minDiff, diff);
         }
 
         return minDiff;
+    }
+
+    public static void main(String[] args) {
+
+        minimumDifference obj = new minimumDifference();
+
+        int[] nums = {9, 4, 1, 7};
+        int k = 2;
+
+        System.out.println(obj.minimumDifference(nums, k));
     }
 }
---
+++ EASY/nextGreaterElement.java
@@ -0,0 +1,42 @@
+import java.util.*;
+
+public class nextGreaterElement {
+
+    public static int[] nextGreaterElement(int[] nums1, int[] nums2) {
+
+        int[] ans = new int[nums1.length];
+
+        for (int i = 0; i < nums1.length; i++) {
+
+            int idx = -1;
+
+            for (int j = 0; j < nums2.length; j++) {
+                if (nums2[j] == nums1[i]) {
+                    idx = j;
+                    break;
+                }
+            }
+
+            ans[i] = -1;
+
+            for (int j = idx + 1; j < nums2.length; j++) {
+                if (nums2[j] > nums1[i]) {
+                    ans[i] = nums2[j];
+                    break;
+                }
+            }
+        }
+
+        return ans;
+    }
+
+    public static void main(String[] args) {
+
+        int[] nums1 = {4, 1, 2};
+        int[] nums2 = {1, 3, 4, 2};
+
+        int[] result = nextGreaterElement(nums1, nums2);
+
+        System.out.println(Arrays.toString(result));
+    }
+}
