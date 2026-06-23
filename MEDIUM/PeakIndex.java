75b4704f9bca919c3841fc6ba01399ddab5ccf27
khushisharma2004 <ks969250@gmail.com>
2026-06-24 00:56:41 +0530

    852. Peak Index in a Mountain Array

---
+++ MEDIUM/PeakIndex.java
@@ -0,0 +1,27 @@
+public class PeakIndex {
+
+    public int peakIndexInMountainArray(int[] arr) {
+        int lo = 1;
+        int hi = arr.length - 2;
+
+        while (lo < hi) {
+            int mid = lo + (hi - lo) / 2;
+
+            if (arr[mid] > arr[mid + 1] && arr[mid] > arr[mid - 1]) {
+                return mid;
+            } else if (arr[mid] > arr[mid - 1] && arr[mid] < arr[mid + 1]) {
+                lo = mid + 1;
+            } else {
+                hi = mid - 1;
+            }
+        }
+        return lo;
+    }
+
+    public static void main(String[] args) {
+        PeakIndex obj = new PeakIndex();
+        int[] arr = {0, 2, 5, 3, 1};
+
+        System.out.println(obj.peakIndexInMountainArray(arr));
+    }
+}
