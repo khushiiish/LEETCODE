916e6082a6e8187e1710384804c70b914a350b9f
khushi sharma <ks969250@gmail.com>
2026-06-29 23:22:05 +0530

    49 Group Anagrams

---
+++ MEDIUM/GroupAnagram.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+public class GroupAnagram {
+
+    public List<List<String>> groupAnagrams(String[] strs) {
+        Map<String, List<String>> map = new HashMap<>();
+
+        for (String s : strs) {
+            char[] arr = s.toCharArray();
+            Arrays.sort(arr);
+            String key = new String(arr);
+
+            if (!map.containsKey(key)) {
+                map.put(key, new ArrayList<>());
+            }
+
+            map.get(key).add(s);
+        }
+
+        return new ArrayList<>(map.values());
+    }
+
+    public static void main(String[] args) {
+        GroupAnagram obj = new GroupAnagram();
+
+        String[] strs = {"eat", "tea", "tan", "ate", "nat", "bat"};
+
+        List<List<String>> result = obj.groupAnagrams(strs);
+
+        System.out.println(result);
+    }
+}
