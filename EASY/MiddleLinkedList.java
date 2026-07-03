47f9ef40189dd652d33f00e9422106d8eff1b03f
khushi sharma <ks969250@gmail.com>
2026-07-03 23:24:39 +0530

    876. Middle of the Linked List

---
+++ EASY/MiddleLinkedList.java
@@ -0,0 +1,44 @@
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {}
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
+
+public class MiddleLinkedList {
+
+    public ListNode middleNode(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        return slow;
+    }
+
+    public static void main(String[] args) {
+        // Create linked list: 1 -> 2 -> 3 -> 4 -> 5
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(3);
+        head.next.next.next = new ListNode(4);
+        head.next.next.next.next = new ListNode(5);
+
+        MiddleLinkedList obj = new MiddleLinkedList();
+        ListNode middle = obj.middleNode(head);
+
+        System.out.println("Middle Node: " + middle.val);
+    }
+}
