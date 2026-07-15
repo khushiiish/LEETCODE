77259735eff7abb009fb8fa23bb4af79ea03adae
khushi sharma <ks969250@gmail.com>
2026-07-15 22:41:36 +0530

    796. Rotate String

---
+++ EASY/RotateString.java
@@ -0,0 +1,19 @@
+public class RotateString {
+
+    public static boolean rotateString(String s, String goal) {
+        if (s.length() != goal.length()) {
+            return false;
+        }
+
+        return (s + s).contains(goal);
+    }
+
+    public static void main(String[] args) {
+        String s = "abcde";
+        String goal = "cdeab";
+
+        boolean result = rotateString(s, goal);
+
+        System.out.println("Is rotation? " + result);
+    }
+}
