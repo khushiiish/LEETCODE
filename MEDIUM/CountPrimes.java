d7fb8adc4f442ba8fdc1bf4d649cc820fe2d23e7
khushi sharma <ks969250@gmail.com>
2026-08-08 02:24:23 +0530

    204. Count Primes

---
+++ MEDIUM/CountPrimes.java
@@ -0,0 +1,39 @@
+import java.util.Arrays;
+
+public class CountPrimes {
+
+    public static int countPrimes(int n) {
+        if (n <= 2)
+            return 0;
+
+        boolean[] prime = new boolean[n];
+        Arrays.fill(prime, true);
+
+        prime[0] = prime[1] = false;
+
+        for (int i = 2; i * i < n; i++) {
+            if (prime[i]) {
+                for (int j = i * i; j < n; j += i) {
+                    prime[j] = false;
+                }
+            }
+        }
+
+        int count = 0;
+
+        for (int i = 2; i < n; i++) {
+            if (prime[i])
+                count++;
+        }
+
+        return count;
+    }
+
+    public static void main(String[] args) {
+        int n = 10;
+
+        int result = countPrimes(n);
+
+        System.out.println("Number of primes less than " + n + " = " + result);
+    }
+}
